caa7fc6f80fa3fd6424794c6fe66f84eed0c95f8
meeenalll411 <meeenalll411@gmail.com>
2026-09-21 13:43:14 +0530

    Day 24-Q47

---
+++ Day 24/Q47.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int row, column;
+
+    for (row = 1; row <= 5; row++)
+    {
+        for (column = 1; column <= row; column++)
+        {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
