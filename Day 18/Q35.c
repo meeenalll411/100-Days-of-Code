b89fa70779565a201a031be4eb123a1fb0bd1b9b
meeenalll411 <meeenalll411@gmail.com>
2026-09-21 13:32:07 +0530

    Day 18-Q35

---
+++ Day 18/Q35.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int number, i;
+
+    printf("Enter a positive integer: ");
+    scanf("%d", &number);
+
+    if (number <= 0)
+    {
+        printf("Please enter a positive integer.\n");
+    }
+    else
+    {
+        printf("Factors of %d are: ", number);
+
+        for (i = 1; i <= number; i++)
+        {
+            if (number % i == 0)
+            {
+                printf("%d ", i);
+            }
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
