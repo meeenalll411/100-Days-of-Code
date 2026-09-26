2d0b98a6753e82ced5fbedf4cc778faeca70e3bc
meeenalll411 <meeenalll411@gmail.com>
2026-09-26 17:20:39 +0530

    Day 47-Q94

---
+++ Day 47/Q94.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char sentence[200];
+    char longest[100] = "";
+    char word[100];
+    int i = 0, j = 0;
+    int maxLength = 0;
+
+    printf("Enter a sentence: ");
+    fgets(sentence, sizeof(sentence), stdin);
+
+    while (1)
+    {
+        if (sentence[i] != ' ' &&
+            sentence[i] != '\n' &&
+            sentence[i] != '\0')
+        {
+            word[j++] = sentence[i];
+        }
+        else
+        {
+            word[j] = '\0';
+
+            if (j > maxLength)
+            {
+                maxLength = j;
+                strcpy(longest, word);
+            }
+
+            j = 0;
+
+            if (sentence[i] == '\0')
+                break;
+        }
+
+        i++;
+    }
+
+    printf("Longest word: %s\n", longest);
+
+    return 0;
+}
